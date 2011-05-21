b303d46d3a28a2449dd8f33d2e98d9e48dc19c63
Denis <denis.labaye@gmail.com>
2011-05-21 16:49:32 +0200

    initied clojure code

---
+++ src/enlivesamples/sample1.clj
@@ -0,0 +1,41 @@
+(ns enlivesamples.sample1
+  (:require [net.cgrand.enlive-html :as html])
+  (:use [net.cgrand.moustache :only [app]]
+        [enlivesamples.utils :only [run-server render-to-response]]))
+
+;; ----------------------------------------------------------------------------
+;; Code adapted from https://github.com/swannodette/enlive-tutorial
+;; ----------------------------------------------------------------------------
+
+(html/deftemplate index "enlivesamples/sample1.html"
+  [ctxt]
+  [:p#message] (html/content (if-let [msg (:message ctxt)]
+                               msg
+                               "you forgot to put a content"))
+  [:p#fooId] (html/content (:fooId ctxt)))
+
+;; ========================================
+;; The App
+;; ========================================
+
+(def routes
+  (app
+   [""]       (fn [req] (render-to-response
+                        (index {:message (str "Date is: " (java.util.Date.))})))
+   ["change"] (fn [req] (render-to-response
+                        (index {
+                                :fooId "I was here ;)"})))
+   ["date"] (fn [req] (render-to-response
+                      (index {:message (str "Today is: "
+                                            (java.util.Date.))})))
+   [&]        {:status 404
+               :body "Page Not Found"}))
+ 
+(defonce *server* (run-server routes))
+
+'(do (require 'enlivesamples.sample1)
+    (in-ns 'enlivesamples.sample1)
+    (use ['clojure.contrib.repl-utils :only ['show]]
+         ['clojure.test :only '[run-tests]]
+         ['clojure.repl]
+         ['enlivesamples.utils]))
